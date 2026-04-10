b36e05707f4f5c3e67948e7249187c1e44f83c73
purvarishX <purvawaldey97@gmail.com>
2026-04-10 10:22:48 +0530

    events html, js

---
+++ events.js
@@ -0,0 +1,36 @@
+function readTextValue() {
+    alert("Button Click");
+}
+
+function changeText() {
+    console.log("Change value");
+}
+
+function onCityChange() {
+    alert('City Change');
+}
+
+function clickButton(event, language) {
+    alert(language);
+    console.log(event.target, "Event");
+}
+
+function onMouseEnterDiv() {
+    console.log("Mouse Enter");
+}
+
+function onMouseLeaveDiv() {
+    console.log("Mouse Leave");
+}
+
+function onKeyDown() {
+    console.log("KEY DOWN")
+}
+
+function onPageScroll() {
+    console.log("On Scroll div")
+}
+
+function resizeLog() {
+    console.log('Rezise page');
+}
